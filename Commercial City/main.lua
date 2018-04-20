8f0304f12a5f1db9aac8be312421d3bd2c43426e
Abdou23 <abdelghafour.mohie@gmail.com>
2018-04-20 04:29:08 +0200

    Empty Project

---
+++ Commercial City/main.lua
@@ -0,0 +1,18 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+-- Your code here
+
+display.setStatusBar( display.HiddenStatusBar )
+display.setDefault( "background", 1,1,1 )
+
+screen = require ( "Utils.screen")
+math2 = require ("Utils.math2")
+color = require ("Utils.color")
+
+
+local composer = require ("composer")
+composer.gotoScene( "GameScene"  )
